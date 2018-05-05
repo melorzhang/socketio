a15faab3bddb93e0558c62e007b592206dceee19
Melor Z <497112718@qq.com>
2018-05-05 13:18:05 +0800

    #

--- config/errCfg.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by hp on 2018/5/3.
- */
-module.exports = {
-	err999: "invalid username/password",
-	err998: "invalid token"
-};
--- config/msgCfg.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by hp on 2018/5/3.
- */
-module.exports = {
-	newJoin: "someone join this room",
-	originNotAllow: "orgin not allow",
-};
