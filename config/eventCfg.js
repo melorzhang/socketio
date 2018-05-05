b702fe5f85f138b37fdc2ff1a2868fad5a283eed
Melor Z <497112718@qq.com>
2018-05-05 23:22:52 +0800

    Revert "#"
    This reverts commit a15faab3bddb93e0558c62e007b592206dceee19.

---
+++ config/eventCfg.js
@@ -0,0 +1,13 @@
+/**
+ * Created by hp on 2018/5/3.
+ */
+module.exports = {
+	sysMsg: "sysMsg",
+	sysMsgService:'sysMsgService',
+	getToken: "getToken",
+	chatMsg: "chat message",
+	err:"err",
+	login:"login",
+	qryOnlineCustomers:'qryOnlineCustomers',
+	getOnlineCustomer:'getOnlineCustomer',
+};
